0fb9cf496b2e9f4c7c93b60a77bc27c6a714af3b
IvanFrezzaJr <ivanfrezza@gmail.com>
2025-03-12 22:57:48 +0000

    refactor: move db logic to the repositories

--- Data/BookingRepository.cs
+++ Data/BookingRepository.cs
@@ -3,9 +3,9 @@ using CAGolfClubDB.Models;
 
 namespace CAGolfClubDB.Data;
 
-public class BookingRepository : Repository<Booking>
+public class BookingRepository(CAGolfClubDBContext context) : Repository<Booking>(context)
 {
-    private readonly CAGolfClubDBContext _context;
+    private readonly CAGolfClubDBContext _context = context;
 
     public IQueryable<Booking> GetFilteredBookings()
     {
@@ -14,13 +14,67 @@ public class BookingRepository : Repository<Booking>
         return query;
     }
 
-    public BookingRepository(CAGolfClubDBContext context) : base(context)
-    {
-        _context = context;
-    }
-
     public async Task<List<Booking>> GetBookingsAsync()
     {
         return await _context.Booking.OrderBy(p => p.Date).ToListAsync();
+    }
+
+
+    public async Task<bool> IsSlotAvailableAsync(string slot, DateTime date)
+    {
+        var slotAvailable = await _context.Booking
+            .Where(e => e.Date.Date == date.Date && e.Slot == slot)
+            .FirstOrDefaultAsync();
+        return slotAvailable == null;
+    }
+
+    public async Task<bool> IsPlayerAlreadyBookedAsync(int playerId, DateTime date)
+    {
+        var alreadyBooked = await _context.Booking
+            .Where(e => e.PlayerId == playerId && e.Date.Date == date.Date)
+            .FirstOrDefaultAsync();
+        return alreadyBooked != null;
+    }
+
+    public async Task AddBookingAsync(Booking booking, List<BookingPlayer> bookingPlayers)
+    {
+        using var transaction = await _context.Database.BeginTransactionAsync();
+        try
+        {
+            _context.Booking.Add(booking);
+            await _context.SaveChangesAsync();
+
+            foreach (var bookingPlayer in bookingPlayers)
+            {
+                bookingPlayer.BookingId = booking.Id;
+                _context.BookingPlayer.Add(bookingPlayer);
+            }
+
+            await _context.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync();
+            throw new Exception("Error adding booking", ex);
+        }
+    }
+
+    public List<string> GenerateAvailableTimes()
+    {
+        var times = new List<string>();
+        var slotTime = new DateTime(1, 1, 1, 0, 0, 0);
+        var currentTime = DateTime.Now;
+
+        while (slotTime.Hour != 23 || slotTime.Minute != 45)
+        {
+            times.Add(slotTime.ToString("HH:mm"));
+            slotTime = slotTime.AddMinutes(15); // Advances by 15 minutes
+        }
+
+        slotTime.AddMinutes(15);
+        times.Add(slotTime.ToString("HH:mm"));
+
+        return times;
     }
 }
